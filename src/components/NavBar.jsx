5218067178e0b4f7fbc8ce77f2162daab777653d
Fidel Castro <fc.fidel1995@gmail.com>
2022-01-02 19:12:37 -0500

    basic templating done

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@
 const NavBar = () => {
     return (
         <nav class="flex flex-col px-4 mt-10 text-center">
-        <a href="/" className="py-2 text-sm text-gray-700 bg-gray-200 rounded dark:text-gray-100 dark:bg-gray-800">Overview</a>
+        <a href="/about" className="py-2 text-sm text-gray-700 bg-gray-200 rounded dark:text-gray-100 dark:bg-gray-800">About</a>
         <a href="/" className="py-2 mt-3 text-sm text-gray-600 rounded dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-800">Tickets</a>
         <a href="/" className="py-2 mt-3 text-sm text-gray-600 rounded dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-800">Ideas</a>
         <a href="/" className="py-2 mt-3 text-sm text-gray-600 rounded dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-800">Contacts</a>
---
+++ src/routes/About.jsx
@@ -0,0 +1,5 @@
+export default function About() {
+    return (
+        <div className="bg-blue-500">dssds</div>
+    );
+  }
